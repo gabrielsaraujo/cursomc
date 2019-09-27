226ab7cfd2629a9bb8b88caf7e55403c2f1bbedb
Gabriel Araujo <gabriellsaaraujo@gmail.com>
2019-09-27 12:19:31 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/gabrielaraujo/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.gabrielaraujo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gabrielaraujo.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+	
+
+}
